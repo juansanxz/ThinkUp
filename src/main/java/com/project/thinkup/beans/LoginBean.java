27f9a5a04ef3804b7f3526d7bd506bf060488fb4
unknown <andres.onate@labinfo.is.escuelaing.edu.co>
2023-04-21 14:58:37 -0500

    LoginBean

---
+++ src/main/java/com/project/thinkup/beans/LoginBean.java
@@ -0,0 +1,32 @@
+package com.project.thinkup.beans;
+
+import javax.faces.bean.ManagedBean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Component
+@ManagedBean
+@SessionScope
+public class LoginBean {
+	
+    private String username;
+    private String password;
+
+    // getters y setters
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
