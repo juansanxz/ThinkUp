72f49801613048825a424a286cd02dfa48291e81
unknown <juan.sanchez-pe@labinfo.is.escuelaing.edu.co>
2023-04-28 15:33:03 -0500

    Botones columna izquierda de main, y columnas de info de ideas

--- src/main/java/com/project/thinkup/service/ThinkUp.java
+++ src/main/java/com/project/thinkup/service/ThinkUp.java
@@ -56,7 +56,7 @@ public class ThinkUp {
 	public String login(String username, String password) {
 		if (loginBean.login(username, password)) {
 			currentUser = loginBean.getUser();
-			return "main.xhtml";
+			return "main.xhtml?faces-redirect=true";
 		} else {
 			FacesContext context = FacesContext.getCurrentInstance();
 			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "El usuario o contraseña es erroneo",
