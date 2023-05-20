9f1fbb966ccddf266a37f9dd912b42d2184b6222
Andres Oñate <andrescamiloquimbayo@gmail.com>
2023-05-20 09:41:07 -0500

    v1 (#46)

--- src/main/java/com/project/thinkup/beans/ChartJsBean.java
+++ src/main/java/com/project/thinkup/beans/ChartJsBean.java
@@ -1,10 +1,17 @@
 package com.project.thinkup.beans;
 
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import com.project.thinkup.service.IdeaService;
 
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
 import org.primefaces.model.chart.BarChartModel;
+import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.chart.DateAxis;
+import org.primefaces.model.chart.LineChartSeries;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,7 +22,7 @@ import org.springframework.stereotype.Component;
 
 @ManagedBean
 @Component
-@ApplicationScoped
+@RequestScoped
 
 public class ChartJsBean {
 
@@ -26,10 +33,51 @@ public class ChartJsBean {
     @Autowired
     IdeaService ideaService;
 
+    @PostConstruct
+    public void init() {
+        createModelArea();
+
+    }
 
     private void createModelArea(){
         modelArea = new BarChartModel();
+        ChartSeries student = new ChartSeries();
+        student.setLabel("Estudiantes");
+        Long ideasByStudent = ideaService.countIdeasByUserArea("estudiante");
+        student.set("Estudiantes", ideasByStudent);
 
+        ChartSeries teacher = new ChartSeries();
+        teacher.setLabel("Profesores");
+        Long ideasByTeacher = ideaService.countIdeasByUserArea("profesor");
+        teacher.set("Profesores", ideasByTeacher);
+
+        ChartSeries adminis = new ChartSeries();
+        adminis.setLabel("Administrativos");
+        Long ideasByAdminis = ideaService.countIdeasByUserArea("administrativo");
+        adminis.set("Profesores", ideasByAdminis);
+
+        ChartSeries externos = new ChartSeries();
+        externos.setLabel("Externo");
+        Long ideasByExternos = ideaService.countIdeasByUserArea("externo");
+        externos.set("Externos", ideasByExternos);
+
+
+        modelArea.addSeries(student);
+        modelArea.addSeries(teacher);
+        modelArea.addSeries(adminis);
+        modelArea.addSeries(externos);
+
+        modelArea.setTitle("Estadísticas por área");
+        modelArea.setLegendPosition("ne");
+       
+        Axis xAxis = modelArea.getAxis(AxisType.X);
+        xAxis.setLabel("Areas");
+       
+        Axis yAxis = modelArea.getAxis(AxisType.Y);
+        yAxis.setLabel("Ideas");
+        yAxis.setMin(0);
+        yAxis.setMax(5);
+       
     }
 
 
