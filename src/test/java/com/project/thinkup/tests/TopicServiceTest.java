9a3031248fdbdba090ec855738a61f230f566d3f
Juan Pablo Poveda <juan.canon1725@gmail.com>
2023-05-26 22:48:14 -0500

    test 42

--- src/test/java/com/project/thinkup/tests/TopicServiceTest.java
+++ src/test/java/com/project/thinkup/tests/TopicServiceTest.java
@@ -3,13 +3,24 @@ package com.project.thinkup.tests;
 import com.project.thinkup.model.Topic;
 import com.project.thinkup.repository.TopicRepository;
 import com.project.thinkup.service.TopicService;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.mockito.*;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class TopicServiceTest {
+
+    @Mock
+    private TopicRepository topicRepository;
+
+    @InjectMocks
+    private TopicService topicService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     void shouldReturnAddedTopic() {
@@ -38,5 +49,39 @@ class TopicServiceTest {
         assertEquals(topic, result);
     }
 
+    @Test
+    void shouldDeletesTopic() {
+        Long topicId = 1L;
+        doNothing().when(topicRepository).deleteById(topicId);
+
+        topicService.deleteTopic(topicId);
+        assertEquals(0, topicRepository.count());
+    }
+
+    @Test
+    void shouldDeletesAllTopics() {
+        doNothing().when(topicRepository).deleteAll();
+
+        topicService.deleteAllTopics();
+        assertEquals(0, topicRepository.count());
+    }
+
+    @Test
+    void shouldExistingTopicReturnsTrue() {
+        String topicTitle = "Test Topic";
+        when(topicRepository.existsByTitle(topicTitle)).thenReturn(true);
+        boolean result = topicService.topicExist(topicTitle);
+
+        assertTrue(result);
+    }
+
+    @Test
+    void shouldNonExistingTopic_ReturnsFalse() {
+        String topicTitle = "Nonexistent Topic";
+        when(topicRepository.existsByTitle(topicTitle)).thenReturn(false);
+        boolean result = topicService.topicExist(topicTitle);
+
+        assertFalse(result);
+    }
 
 }
